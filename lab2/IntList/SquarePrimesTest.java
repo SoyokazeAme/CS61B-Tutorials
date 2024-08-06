5e183ef2b8bc5ba7f0f8d8c9f823b1d5398664bc
MinakazeAme <2417349975@qq.com>
2024-08-06 10:17:14 +0800

    lab2 finished

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,20 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesWhenNoChange() {
+        IntList lst = IntList.of(24, 4, 8, 30);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("24 -> 4 -> 8 -> 30", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimesWhenAllChange() {
+        IntList lst = IntList.of(3, 5, 7, 11);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 25 -> 49 -> 121", lst.toString());
+        assertTrue(changed);
+    }
 }
